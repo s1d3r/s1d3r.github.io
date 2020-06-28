7edb15bce6e2a7d44e3303b91d2df2163beed372
s1d3r <onimetop_1337@mail.ru>
2020-06-28 05:39:11 +0300

    small update

--- js/app.js
+++
@@ -1,8 +0,0 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-
-particlesJS.load('particles-js', 'particlesjs.json', function() {
-  console.log('particles.js loaded - callback');
-});
