84c5036d141d509181f8bcbacf1ff3df39947df5
Soider <57572890+s1d3r@users.noreply.github.com>
2020-09-20 16:24:42 +0300

    owned l3l

--- scripts.js
+++ scripts.js
@@ -9,8 +9,9 @@ tippy('#discord', {
 });
 
 tippy('#telegram', {
-    content: 'Telegram',
+    content: 'Copied to clipboard',
     animation: 'scale',
+    trigger: 'click',
 })
 
 $('#music').prop("volume", 0.5);
@@ -60,3 +61,48 @@ if(currentRange)
         window.getSelection().addRange(currentRange);
     }
 }
+
+function Telegram(){
+    
+    var theText = '@soiderblyat';
+    
+    var hiddenCopy = document.createElement('div');
+        hiddenCopy.innerHTML = theText;
+        hiddenCopy.style.position = 'absolute';
+        hiddenCopy.style.left = '-9999px';
+    
+        var currentRange;
+    
+    if(document.getSelection().rangeCount > 0)
+        {
+            currentRange = document.getSelection().getRangeAt(0);
+            window.getSelection().removeRange(currentRange);
+        }
+        else
+        {
+            currentRange = false;
+    }
+    
+    document.body.appendChild(hiddenCopy);
+    var CopyRange = document.createRange();
+    CopyRange.selectNode(hiddenCopy);
+    window.getSelection().addRange(CopyRange);
+    
+    try
+        {
+            document.execCommand('copy');
+        }
+    
+    catch(err)
+        {
+             window.alert("Your Browser Doesn't support this! Error : " + err);
+        }
+    
+    window.getSelection().removeRange(CopyRange);
+    document.body.removeChild(hiddenCopy);
+    
+    if(currentRange)
+        {
+            window.getSelection().addRange(currentRange);
+        }
+    }
